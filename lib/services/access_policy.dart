c86b9e3c3ae9b19719702de2f241e8734839eeb2
matthewcodergamer <gamerstriper@gmail.com>
2026-09-03 06:55:26 -0400

    Add pharmacist clinical permissions

--- lib/services/access_policy.dart
+++ lib/services/access_policy.dart
@@ -1,6 +1,18 @@
 import '../models.dart';
 
-enum ClinicalAction { viewPatient, createEncounter, assignPatient, createMedicationOrder, administerMedication }
+enum ClinicalAction {
+  viewPatient,
+  createEncounter,
+  assignPatient,
+  createMedicationOrder,
+  administerMedication,
+  receiveMedicationStock,
+  verifyMedicationProduct,
+  dispenseMedication,
+  viewInventory,
+  searchRecalls,
+  generateUnitDoseLabel,
+}
 
 class AccessPolicy {
   const AccessPolicy._();
@@ -8,10 +20,16 @@ class AccessPolicy {
   static bool allows(StaffRole role, ClinicalAction action) {
     return switch ((role, action)) {
       (_, ClinicalAction.viewPatient) => true,
-      (_, ClinicalAction.createEncounter) => true,
+      (StaffRole.doctor || StaffRole.nurse, ClinicalAction.createEncounter) => true,
       (StaffRole.doctor, ClinicalAction.assignPatient) => true,
       (StaffRole.doctor, ClinicalAction.createMedicationOrder) => true,
       (StaffRole.nurse, ClinicalAction.administerMedication) => true,
+      (StaffRole.pharmacist, ClinicalAction.receiveMedicationStock) => true,
+      (StaffRole.pharmacist, ClinicalAction.verifyMedicationProduct) => true,
+      (StaffRole.pharmacist, ClinicalAction.dispenseMedication) => true,
+      (StaffRole.pharmacist || StaffRole.nurse, ClinicalAction.viewInventory) => true,
+      (StaffRole.pharmacist, ClinicalAction.searchRecalls) => true,
+      (StaffRole.pharmacist, ClinicalAction.generateUnitDoseLabel) => true,
       _ => false,
     };
   }
